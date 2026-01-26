d359f3868ce424e797e64f3b8e2f248972322203
Arthur Koucher <arthurkoucher@precisioninno.com>
2026-01-26 13:23:05 +0000

    new changes for the experiment
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- flow/runCorrelationExperiments.sh
+++ flow/runCorrelationExperiments.sh
@@ -31,12 +31,12 @@ function run_from_mpl {
 
   initialize_flow_files $mpl_pause_step
 
-  make do-2_2_floorplan_macro do-2_3_floorplan_tapcell do-2_4_floorplan_pdn do-2_floorplan do-2_floorplan.sdc do-place do-cts do-route do-finish
+  make do-2_2_floorplan_macro do-2_3_floorplan_tapcell do-2_4_floorplan_pdn do-2_floorplan do-2_floorplan.sdc do-place
 }
 
 design="gf12/ariane"
 export DESIGN_CONFIG=./designs/${design}/config.mk
-#export RTLMP_DATA_FLOW_DRIVEN=0
+export RTLMP_DATA_FLOW_DRIVEN=0
 #export RTLMP_TIMING_DRIVEN=1
 
 make synth
